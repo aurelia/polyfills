941a892f8a63bce8dea3566c97e911ee31622359
Rob Eisenberg <rob@bluespire.com>
2016-03-08 13:46:35 -0500

    fix(object): assign ignores null or undefined

--- src/object.js
+++ src/object.js
@@ -54,27 +54,25 @@
 }());
 
 (function (O) {
-  if ('assign' in O) return;
-  O.defineProperty(
-    O,
-    'assign',
-    {
+  if ('assign' in O) {
+    return;
+  }
+
+  O.defineProperty(O, 'assign', {
       configurable: true,
       writable: true,
-      value: (function () {
-        var
-          gOPS = O.getOwnPropertySymbols,
-          // shortcut without explicitly passing through prototype
-          pIE = O.propertyIsEnumerable,
-          filterOS = gOPS ?
-            function (self) {
-              return gOPS(self).filter(pIE, self);
-            } :
-            function () {
-              // just empty Array won't do much within a .concat(...)
-              return Array.prototype;
-            }
-        ;
+      value: (function() {
+        var gOPS = O.getOwnPropertySymbols,
+            // shortcut without explicitly passing through prototype
+            pIE = O.propertyIsEnumerable,
+            filterOS = gOPS ?
+              function (self) {
+                return gOPS(self).filter(pIE, self);
+              } : function () {
+                // just empty Array won't do much within a .concat(...)
+                return Array.prototype;
+              };
+
         return function assign(where) {
           // Object.create(null) and null objects in general
           // might not be fully compatible with Symbols libraries
@@ -83,27 +81,25 @@
           // if you know what you are doing ... so ....
           if (gOPS && !(where instanceof O)) {
             console.warn('problematic Symbols', where);
-            // ... now this script does its bloody business !!!
+            // ... now this script does its business !!!
           }
           // avoid JSHint "don't make function in loop"
           function set(keyOrSymbol) {
             where[keyOrSymbol] = arg[keyOrSymbol];
           }
           // the loop
-          for (var
-            arg,
-            i = 1; i < arguments.length; i++
-          ) {
-            arg = arguments[i];
-            O
-              .keys(arg)
-              .concat(filterOS(arg))
-              .forEach(set)
-            ;
+          for (var i = 1, ii = arguments.length; i < ii; ++i) {
+            var arg = arguments[i];
+
+            if (arg === null || arg === undefined) {
+              continue;
+            }
+
+            O.keys(arg).concat(filterOS(arg)).forEach(set);
           }
+
           return where;
         };
       }())
-    }
-  );
+    });
 }(Object));
