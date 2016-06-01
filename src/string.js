bf04be5bf6497a37e6c39fd4e914268b936f9821
Florian <grimmuko@googlemail.com>
2016-06-01 15:41:29 +0200

    Update string,js
    running on a page that includes MicrosoftAjaxCore - results in a bad polyfill since implemented it without the position parameter for startsWith and endsWith.
    The problem is shown here
    https://jsfiddle.net/floriangrimm/nee9b6kh/2/

--- src/string.js
+++ src/string.js
@@ -1,4 +1,4 @@
-if (!String.prototype.endsWith) {
+if ((!String.prototype.endsWith) || ((function() { try { return !("ab".endsWith("a",1)); } catch (e) { return true; } } )())) {
   String.prototype.endsWith = function(searchString, position) {
     let subjectString = this.toString();
     if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
@@ -10,7 +10,7 @@ if (!String.prototype.endsWith) {
   };
 }
 
-if (!String.prototype.startsWith) {
+if ((!String.prototype.startsWith) || ((function() { try { return !("ab".startsWith("b", 1)); } catch (e) { return true; } } )())) {
   String.prototype.startsWith = function(searchString, position){
     position = position || 0;
     return this.substr(position, searchString.length) === searchString;
