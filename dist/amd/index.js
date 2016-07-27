ab841d3fc22dd743ffb807d1590f40397ea37f44
Rob Eisenberg <rob@bluespire.com>
2016-07-27 10:36:43 -0400

    commit: chore(all): update build

--- dist/amd/index.js
+++ dist/amd/index.js
@@ -5,7 +5,7 @@ define(['exports', './aurelia-polyfills'], function (exports, _aureliaPolyfills)
     value: true
   });
   Object.keys(_aureliaPolyfills).forEach(function (key) {
-    if (key === "default") return;
+    if (key === "default" || key === "__esModule") return;
     Object.defineProperty(exports, key, {
       enumerable: true,
       get: function () {
--- dist/commonjs/index.js
+++ dist/commonjs/index.js
@@ -7,7 +7,7 @@ Object.defineProperty(exports, "__esModule", {
 var _aureliaPolyfills = require('./aurelia-polyfills');
 
 Object.keys(_aureliaPolyfills).forEach(function (key) {
-  if (key === "default") return;
+  if (key === "default" || key === "__esModule") return;
   Object.defineProperty(exports, key, {
     enumerable: true,
     get: function get() {
