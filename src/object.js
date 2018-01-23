5b2cebe78b15172e4c76dff1a873a7c90136d568
bigopon.777@gmail.com <bigopon.777@gmail.com>
2018-01-23 21:21:55 +1100

    feat(Object): add Object.is()

--- src/object.js
+++ src/object.js
@@ -106,4 +106,21 @@ if (typeof FEATURE_NO_ES2015 === 'undefined') {
     });
 }(Object));
 
+/**
+ * Object.is() polyfill
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/is
+ */
+if (!Object.is) {
+  Object.is = function(x, y) {
+    // SameValue algorithm
+    if (x === y) { // Steps 1-5, 7-10
+      // Steps 6.b-6.e: +0 != -0
+      return x !== 0 || 1 / x === 1 / y;
+    } else {
+     // Step 6.a: NaN == NaN
+     return x !== x && y !== y;
+    }
+  };
+}
+
 } // endif FEATURE_NO_ES2015
